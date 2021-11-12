f7bde63ba289e7eebe8a1f307019e1339615e24d
Tescalus <93953487+Tescalus@users.noreply.github.com>
2021-11-12 15:19:10 -0500

    Update John Doe.lua

--- John Doe.lua
+++ John Doe.lua
@@ -14,7 +14,7 @@ loadstring(game:GetObjects("rbxassetid://5325226148")[1].Source)()
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
 if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
 game:GetService("RunService").Heartbeat:connect(function()
-v.Velocity = Vector3.new(0,35,0)
+v.Velocity = Vector3.new(0,30,0)
 wait(0.5)
 end)
 end
@@ -22,7 +22,7 @@ end
 warn("Netless Activated!")
  -- FLING BELOW!
 
-power = 65000 -- The lower the number, the more stable the torso is, but less fling power.
+power = 60000 -- The lower the number, the more stable the torso is, but less fling power.
 
 game:GetService('RunService').Stepped:connect(function()
 game.Players.LocalPlayer.Character.Head.CanCollide = false
