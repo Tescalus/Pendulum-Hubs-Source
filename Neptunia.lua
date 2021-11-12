782c20928e43f81e05119cdab24e6b0838f834e4
Tescalus <93953487+Tescalus@users.noreply.github.com>
2021-11-12 15:20:09 -0500

    Update Neptunia.lua

--- Neptunia.lua
+++ Neptunia.lua
@@ -11,7 +11,7 @@ end
 warn("Netless Activated!")
  -- FLING BELOW!
 
-power = 65000 -- The lower the number, the more stable the torso is, but less fling power.
+power = 60000 -- The lower the number, the more stable the torso is, but less fling power.
 
 game:GetService('RunService').Stepped:connect(function()
 game.Players.LocalPlayer.Character.Head.CanCollide = false
