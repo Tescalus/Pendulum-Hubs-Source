9cd38479dc17a3f00f8bad273bebe4b2a5dca302
Tescalus <93953487+Tescalus@users.noreply.github.com>
2021-11-12 14:55:29 -0500

    Update krystal dance.lua

--- krystal dance.lua
+++ krystal dance.lua
@@ -6,7 +6,7 @@ loadstring(game:GetObjects("rbxassetid://4480871791")[1].Source)()
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
 if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
 game:GetService("RunService").Heartbeat:connect(function()
-v.Velocity = Vector3.new(0,30,0)
+v.Velocity = Vector3.new(0,35,0)
 wait(0.5)
 end)
 end
