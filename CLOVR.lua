432e8ee3604910f1c53381ce5b1e6fb6b4fb3f7d
Tescalus <93953487+Tescalus@users.noreply.github.com>
2021-11-24 18:54:36 -0500

    Update CLOVR.lua

--- CLOVR.lua
+++ CLOVR.lua
@@ -16,7 +16,7 @@ end)
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
 if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
 game:GetService("RunService").Heartbeat:connect(function()
-v.Velocity = Vector3.new(0,35,0)
+v.Velocity = Vector3.new(0,30,0)
 wait(0.5)
 end)
 end
