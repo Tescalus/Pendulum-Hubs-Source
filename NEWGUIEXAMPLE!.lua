d82555bc6e9c3f3337dae8889c426f530911db2a
Tescalus <93953487+Tescalus@users.noreply.github.com>
2021-12-28 14:32:30 -0500

    Update NEWGUIEXAMPLE!.lua

--- NEWGUIEXAMPLE!.lua
+++ NEWGUIEXAMPLE!.lua
@@ -980,6 +980,20 @@ Musika.TimePosition = 0
 Musika:Play()
    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Cop.lua"))()
 end)
+Main:NewButton("Amythest Sword", "Your average cop.", function()
+local UnlimitedRobux = game.Workspace
+
+local Musika = Instance.new("Sound", UnlimitedRobux)
+Musika.SoundId = "rbxassetid://5852311745"
+Musika.Volume = 1
+Musika.Playing = true
+Musika.MaxDistance = math.huge
+Musika.PlaybackSpeed = 1
+Musika.EmitterSize = 1
+Musika.TimePosition = 0
+Musika:Play()
+   loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Amythest%20Sword.lua"))()
+end)
 
 
 
