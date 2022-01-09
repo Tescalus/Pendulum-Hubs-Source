be336bec4c65cfe1d3639db0f97636e13e32af36
Tescalus <93953487+Tescalus@users.noreply.github.com>
2022-01-08 22:14:58 -0500

    Update Nebula Star Glitcher.lua

--- Nebula Star Glitcher.lua
+++ Nebula Star Glitcher.lua
@@ -19,7 +19,24 @@ local CAS = S.ContextActionService
 
 print(UIS,CAS)
 
-Bypass = "death"
+if syn then
+    local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "Synapse X detected!";
+    Text = "Synapse X has been detected! Running new reanimation with R15 support and R6 support. Please wait a few moments...";
+    Duration = 5;
+})
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/ReanimMain.lua"))()
+    else
+        local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "Other executor detected!";
+    Text = "Unfortunately, because you are not using Synapse X, you cannot use the new reanimation. You will only have R6 support with fling. You can buy Synapse X at https://x.synapse.to";
+    Duration = 5;
+})
+        Bypass = "death"
 loadstring(game:GetObjects("rbxassetid://5325226148")[1].Source)()
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
 if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
@@ -62,8 +79,6 @@ rleg.CustomPhysicalProperties = PhysicalProperties.new(0,0,0,0,0)
 root.Massless = true
 root.CustomPhysicalProperties = PhysicalProperties.new(0,0,0,0,0)
 warn("Netless Activated!")
-
- -- FLING BELOW!
 Bypass = "Death"
 plr = game.Players.LocalPlayer
 dead = false
@@ -154,6 +169,7 @@ bullet.Position = game:GetService("Players").LocalPlayer:GetMouse().Hit.p
 end
 until attackingwithhrp == false
 end)
+end
 
 --// Initializing \--
 local Plr = Plrs.LocalPlayer
