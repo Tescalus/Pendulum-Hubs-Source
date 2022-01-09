88515b3ccaad346813cf15e0280b9766dc048875
Tescalus <93953487+Tescalus@users.noreply.github.com>
2022-01-08 21:55:47 -0500

    Update FEPen.lua

--- FEPen.lua
+++ FEPen.lua
@@ -2,12 +2,25 @@
 Hat to wear: https://www.roblox.com/catalog/4506945409/Corrupt-Demonic-Greatsword
 ]]--
 
-Bypass = "death"
-loadstring(game:GetObjects("rbxassetid://5325226148")[1].Source)()
-plr = game.Players.LocalPlayer
-dead = false
-char = plr.Character
+if syn then
+    local CoreGui = game:GetService("StarterGui")
 
+CoreGui:SetCore("SendNotification", {
+    Title = "Synapse X detected!";
+    Text = "Synapse X has been detected! Running new reanimation with R15 support and R6 support. Please wait a few moments...";
+    Duration = 5;
+})
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/ReanimMain.lua"))()
+    else
+        local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "Other executor detected!";
+    Text = "Unfortunately, because you are not using Synapse X, you cannot use the new reanimation. You will only have R6 support with fling. You can buy Synapse X at https://x.synapse.to";
+    Duration = 5;
+})
+        Bypass = "death"
+loadstring(game:GetObjects("rbxassetid://5325226148")[1].Source)()
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
 if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
 game:GetService("RunService").Heartbeat:connect(function()
@@ -139,13 +152,14 @@ bullet.Position = game:GetService("Players").LocalPlayer:GetMouse().Hit.p
 end
 until attackingwithhrp == false
 end)
+end
 
 
 -----------------------------------------------------------------------------------
 
 local plrs = game:GetService("Players")
 local plr = plrs.LocalPlayer
-local char = workspace.non
+local char = game.Workspace.non
 local tweenserv = game:GetService("TweenService")
 local lighting = game:GetService("Lighting")
 local hum = char:FindFirstChildOfClass("Humanoid")
