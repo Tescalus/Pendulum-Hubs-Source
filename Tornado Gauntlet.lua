82b85f474c2b49375bd7a89e8c64873545c5ce57
Tescalus <93953487+Tescalus@users.noreply.github.com>
2022-01-08 22:52:32 -0500

    Update Tornado Gauntlet.lua

--- Tornado Gauntlet.lua
+++ Tornado Gauntlet.lua
@@ -1,4 +1,21 @@
-Bypass = "death"
+if syn then
+    local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "Synapse X detected!";
+    Text = "Synapse X has been detected! Running new reanimation with R15 support and R6 support. Please wait a few moments...";
+    Duration = 5;
+})
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/ReanimMain.lua"))()
+    else
+        local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "Other executor detected!";
+    Text = "Unfortunately, because you are not using Synapse X, you cannot use the new reanimation. You will only have R6 support with fling. You can buy Synapse X at https://x.synapse.to";
+    Duration = 5;
+})
+        Bypass = "death"
 loadstring(game:GetObjects("rbxassetid://5325226148")[1].Source)()
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
 if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
@@ -38,13 +55,15 @@ lleg.Massless = true
 lleg.CustomPhysicalProperties = PhysicalProperties.new(0,0,0,0,0)
 rleg.Massless = true
 rleg.CustomPhysicalProperties = PhysicalProperties.new(0,0,0,0,0)
+root.Massless = true
+root.CustomPhysicalProperties = PhysicalProperties.new(0,0,0,0,0)
 warn("Netless Activated!")
 Bypass = "Death"
 plr = game.Players.LocalPlayer
 dead = false
 char = plr.Character
 
-game.Workspace.non.Humanoid.WalkSpeed = 30
+
 
 bullet = workspace[plr.Name]["HumanoidRootPart"]
 bullet.Transparency = 1
@@ -129,6 +148,7 @@ bullet.Position = game:GetService("Players").LocalPlayer:GetMouse().Hit.p
 end
 until attackingwithhrp == false
 end)
+end
 loadstring(game:GetObjects("rbxassetid://5425999987")[1].Source)()
 warn'Lunarist Loaded.'
 warn'Do not leak this, or else.'
