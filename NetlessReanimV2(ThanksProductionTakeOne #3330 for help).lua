20afada3ae51c11c43d3782c30475ba5fa9b4862
Tescalus <93953487+Tescalus@users.noreply.github.com>
2022-01-16 18:19:15 -0500

    Update NetlessReanimV2(ThanksProductionTakeOne #3330 for help).lua

--- NetlessReanimV2(ThanksProductionTakeOne #3330 for help).lua
+++ NetlessReanimV2(ThanksProductionTakeOne #3330 for help).lua
@@ -17,8 +17,8 @@ if _G.Collisions == nil then _G.Collisions = true end
 if _G.Network == nil then _G.Network = true end
 if _G.CheckForDeath == nil then _G.CheckForDeath = true end
 if _G.Netless2 == nil then _G.Netless2 = false end
-if _G.Claim2 == nil then _G.Claim2 = true end
-if _G.ExtremeNetless == nil then _G.ExtremeNetless = true end
+if _G.Claim2 == nil then _G.Claim2 = false end
+if _G.ExtremeNetless == nil then _G.ExtremeNetless = false end
 
 settings().Physics.PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.Disabled
 settings().Physics.AllowSleep = false
