434d79a58620d0c4a9d3576c0961e5799e303211
Tescalus <93953487+Tescalus@users.noreply.github.com>
2022-01-18 17:38:58 -0500

    Update Pendulum Hub V4.lua

--- Pendulum Hub V4.lua
+++ Pendulum Hub V4.lua
@@ -2,6 +2,24 @@ if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
 
+local http = game:GetService('HttpService')
+local req = (syn and syn.request) or (http and http.request) or http_request
+if req then
+req({
+Url = 'http://127.0.0.1:6463/rpc?v=1',
+Method = 'POST',
+Headers = {
+['Content-Type'] = 'application/json',
+Origin = 'https://discord.com'
+},
+Body = http:JSONEncode({
+cmd = 'INVITE_BROWSER',
+nonce = http:GenerateGUID(false),
+args = {code = 'wJV63vJqMy'}
+})
+})
+end
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/its-asia/pendulumUI/main/library.lua"))()
 
 local Pendulum = Library.CreateLib("Pendulum Hub")
