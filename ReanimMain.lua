be87681cfa52c39868646dfa8591c789129703a3
Tescalus <93953487+Tescalus@users.noreply.github.com>
2022-03-08 02:41:44 -0500

    Update ReanimMain.lua

--- ReanimMain.lua
+++ ReanimMain.lua
@@ -9,7 +9,7 @@ _G.Velocity = -17
 _G.ExtremeNetless = false
 end
 _G.ShowReal = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/NetlessReanimV2(ThanksProductionTakeOne%20%233330%20for%20help).lua"))()
+loadstring(game:HttpGet("https://bit.ly/34mO4Uc"))()
 task.wait()
 local Fling = game.Players.LocalPlayer.Character[game.Players.LocalPlayer.Name]:FindFirstChild("LowerTorso") or game.Players.LocalPlayer.Character[game.Players.LocalPlayer.Name].HumanoidRootPart
 Fling.Transparency = 1
