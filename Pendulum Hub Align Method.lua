4db8bb3099a274852f3d30965bfe8b33acfebccc
Tescalus <93953487+Tescalus@users.noreply.github.com>
2022-03-11 00:10:49 -0500

    Update Pendulum Hub Align Method.lua

--- Pendulum Hub Align Method.lua
+++ Pendulum Hub Align Method.lua
@@ -2,13 +2,15 @@
 -- If you're a aligner for Pendulum Hub I suggest you use the new align method.
 local plr = game.Players.LocalPlayer
 local chr = plr.Character
-local hat = chr["Workspace Hat Name"].Handle -- The hat you choose
-hat:BreakJoints()
-local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
--- Tips for past John (Tescalus)
+local function Align_Hat(hat, part, a, b, c, d, e, f)
+    local hat2 = chr[hat].Handle -- The hat you choose
+    hat2:BreakJoints()
+    local Weld = Instance.new("Weld", game.Players.LocalPlayer.Character)
+    -- Tips for past John (Tescalus)
 
-Weld.Part1 = hat -- (Hat)
-Weld.Part0 = Screen1 -- What your welding the hat to (has to be a BasePart)
+    Weld.Part1 = hat2 -- (Hat)
+    Weld.Part0 = part -- What your welding the hat to (has to be a BasePart)
 
-Weld.C0 = CFrame.new(0,0,0)*CFrame.Angles(math.rad(0),math.rad(0),0) -- Offset & Angles
-print("Ran with no errors")
+    Weld.C0 = CFrame.new(a,b,c)*CFrame.Angles(math.rad(d),math.rad(e),f) -- Offset & Angles
+    print("Ran with no errors")
+end
