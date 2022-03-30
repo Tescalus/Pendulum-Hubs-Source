acd039b18b5df16a05e5c93f2f26d067430cecd2
Tescalus <93953487+Tescalus@users.noreply.github.com>
2022-03-29 22:29:55 -0400

    Update NetlessReanimV2(ThanksProductionTakeOne #3330 for help).lua

--- NetlessReanimV2(ThanksProductionTakeOne #3330 for help).lua
+++ NetlessReanimV2(ThanksProductionTakeOne #3330 for help).lua
@@ -95,7 +95,6 @@ if not event then
 			cr(cc(function()
 				for i=1,math.max(game:GetService("Stats").Workspace.FPS:GetValue()/27.5,1) do
 					event:Fire()
-					event.Parent = game:GetChildren()[math.random(1,#game:GetChildren())]
 				end
 			end))
 			--task.wait(0/1)
