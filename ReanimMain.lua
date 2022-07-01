09b1b6badbf6268f4a32443613e85e58966c89ca
padero <68622956+shidemuri@users.noreply.github.com>
2022-07-01 18:58:30 -0400

    Update ReanimMain.lua

--- ReanimMain.lua
+++ ReanimMain.lua
@@ -1,5 +1,4 @@
 game:GetService("RunService").Heartbeat:Connect(function()
     workspace.FallenPartsDestroyHeight = -math.huge
 end)
-getgenv()._reanimate = loadstring(game:HttpGet('https://raw.githubusercontent.com/shidemuri/coffeeware/main/reanim.lua'))()
-getgenv()._reanimate()
+return loadstring(game:HttpGet('https://raw.githubusercontent.com/shidemuri/coffeeware/main/reanim.lua'))()
