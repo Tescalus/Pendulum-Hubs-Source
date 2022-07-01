601154741e219de646ddf247daf33661b8356436
CROAXER <93953487+Tescalus@users.noreply.github.com>
2022-07-01 06:04:09 -0400

    Update ReanimMain.lua

--- ReanimMain.lua
+++ ReanimMain.lua
@@ -1,5 +1,5 @@
 game:GetService("RunService").Heartbeat:Connect(function()
     workspace.FallenPartsDestroyHeight = -math.huge
 end)
-if not getgenv()._reanimate then getgenv()._reanimate = loadstring(game:HttpGet('https://raw.githubusercontent.com/Tescalus/bad/main/reanim.lua'))() end
+if not getgenv()._reanimate then getgenv()._reanimate = loadstring(game:HttpGet('https://raw.githubusercontent.com/shidemuri/coffeeware/main/reanim.lua'))() end
 return getgenv()._reanimate()
