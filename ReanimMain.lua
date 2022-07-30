cf49e43b1b4d5d9d0d135fd6c35fd429851cafbf
padero <68622956+shidemuri@users.noreply.github.com>
2022-07-29 22:12:21 -0400

    Update ReanimMain.lua

--- ReanimMain.lua
+++ ReanimMain.lua
@@ -1,5 +1,2 @@
-workspace:GetPropertyChangedSignal("FallenPartsDestroyHeight"):Connect(function()
-    workspace.FallenPartsDestroyHeight = -math.huge
-end)
 if not getgenv()._reanimate then getgenv()._reanimate = loadstring(game:HttpGet('https://raw.githubusercontent.com/shidemuri/coffeeware/main/reanim.lua'))() end
 return getgenv()._reanimate()
